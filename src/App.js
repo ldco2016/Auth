40dea39fb2eb53ed6757bb201f90ffcd6635edb9
danale <danale@danales-MacBook-Pro.local>
2018-03-02 22:15:35 -0500

    Implemented Firebase client setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBj3Jm2ABmLZL_ymQqavTq3MQiLePNXWrg',
+      authDomain: 'auth-7991f.firebaseapp.com',
+      databaseURL: 'https://auth-7991f.firebaseio.com',
+      projectId: 'auth-7991f',
+      storageBucket: 'auth-7991f.appspot.com',
+      messagingSenderId: '95337902664'
+    });
+  }
+
   render() {
     return (
       <View>
