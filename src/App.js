63ceeab4e240aa779cd2546b8b039cb2592c3e3c
danale <danale@danales-MacBook-Pro.local>
2018-03-06 21:33:45 -0500

    Developed logging out user

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
       messagingSenderId: '95337902664'
     });
 
-    firebase.auth().onAuthStateChanged(user => {
+    firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         this.setState({ loggedIn: true });
       } else {
@@ -29,7 +29,9 @@ class App extends Component {
   renderContent() {
     switch (this.state.loggedIn) {
       case true:
-        return <Button>Log Out</Button>;
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+        );
       case false:
         return <LoginForm />;
       default:
